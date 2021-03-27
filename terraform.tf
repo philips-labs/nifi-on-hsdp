9c6798c19a4ccd2e87356403cb4fcdfc6bead2cb
KP <krishna.prasad.srinivasan@philips.com>
2021-03-27 10:10:56 +0530

    update provider versions

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     cloudfoundry = {
       source  = "philips-labs/cloudfoundry"
-      version = ">= 0.1206.0"
+      version = ">= 0.1300.5"
     }
     random = {
       source = "random"
     }
     hsdp = {
       source  = "philips-software/hsdp"
-      version = ">= 0.13.3"
+      version = ">= 0.13.5"
     }
   }
 }
